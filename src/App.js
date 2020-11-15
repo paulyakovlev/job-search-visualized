61aa83aff10cfc2f4b2445ac512903daaeee174a
Paul Yakovlev <yvpavel@gmail.com>
2020-11-14 21:52:59 -0800

    display status data in d3 legend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import {useEffect, useState} from 'react';
+import {Legend} from 'britecharts-react';
+import { fetchStatus } from './api/Status.js';
 import './App.css';
 
 function App() {
+  const [legendState, setLegendState] = useState([]);
+
+  useEffect(() => {
+    let results = []
+    
+    fetchStatus().then((data) => {
+      data.records.forEach(element => {
+        let item = {id:element.fields.id,quantity:element.fields.Totals,name:element.fields.Name};
+        results.push(item);
+      });
+      setLegendState(results);
+      console.log(results);
+    })
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Legend
+        data={legendState}
+        height={250}
+        width={400}
+    />
     </div>
   );
 }
