dc749db5ee5d5cf19995ed0451cdb167ac0fe3a0
Paul Yakovlev <yvpavel@gmail.com>
2020-11-14 22:54:34 -0800

    view sources in d3 table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import {useEffect, useState} from 'react';
-import {Legend} from 'britecharts-react';
-import { fetchStatus } from './api/Status.js';
+import { useEffect, useState } from 'react';
+import { Legend, Bar, Donut } from 'britecharts-react';
+import { fetchStatus,fetchSource } from './api/Status.js';
 import './App.css';
 
 function App() {
   const [legendState, setLegendState] = useState([]);
+  const [barState, setBarState] = useState([]);
 
   useEffect(() => {
-    let results = []
-    
     fetchStatus().then((data) => {
+      let results = []
+
       data.records.forEach(element => {
         let item = {id:element.fields.id,quantity:element.fields.Totals,name:element.fields.Name};
         results.push(item);
       });
+
       setLegendState(results);
       console.log(results);
     })
+
+    fetchSource().then((data) => {
+      let results = []
+      
+      data.records.forEach(element => {
+        let item = {value:element.fields.Totals,name:element.fields.Name};
+        results.push(item);
+      })
+
+      setBarState(results);
+    });
   }, []);
 
   return (
@@ -25,7 +38,19 @@ function App() {
         data={legendState}
         height={250}
         width={400}
-    />
+      />
+      <Bar
+        data={barState}
+        height={400}
+        betweenBarsPadding={0.3}
+        isAnimated={true}
+        margin={{
+            left: 100,
+            right: 40,
+            top: 30,
+            bottom: 40
+        }}
+      />
     </div>
   );
 }
